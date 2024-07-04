8b2146133e236072875eb2285fdc0a62faa0f95e
Tham Tra <tham.tt@morsoftware.com>
2024-07-05 03:05:36 +0700

    [add] https://app.clickup.com/t/86byx41j8

--- lang/en/theme_serlo.php
+++ lang/en/theme_serlo.php
@@ -3,3 +3,40 @@ $string['choosereadme'] = 'Theme photo is a child theme of Boost. It add serlo s
 $string['pluginname'] = 'Serlo';
 $string['secondbutton'] = 'Second Button';
 $string['secondbutton_desc'] = 'This is a description for the second button.';
+
+$string['serlo'] = 'Serlo';
+
+$string['advancedsettings'] = 'Advanced settings';
+// The brand colour setting.                                                                                                        
+$string['brandcolor'] = 'Brand colour';
+// The brand colour setting description.                                                                                            
+$string['brandcolor_desc'] = 'The accent colour.';
+// A description shown in the admin theme selector.                                                                                 
+// Name of the settings pages.                                                                                                      
+$string['configtitle'] = 'Photo settings';
+// Name of the first settings tab.                                                                                                  
+$string['generalsettings'] = 'General settings';
+// The name of our plugin.                                                                                                          
+// Preset files setting.                                                                                                            
+$string['presetfiles'] = 'Additional theme preset files';
+// Preset files help text.                                                                                                          
+$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href=https://docs.moodle.org/dev/Boost_Presets>Boost presets</a> for information on creating and sharing your own preset files, and see the <a href=http://moodle.net/boost>Presets repository</a> for presets that others have shared.';
+// Preset setting.                                                                                                                  
+$string['preset'] = 'Theme preset';
+// Preset help text.                                                                                                                
+$string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';
+// Raw SCSS setting.                                                                                                                
+$string['rawscss'] = 'Raw SCSS';
+// Raw SCSS setting help text.                                                                                                      
+$string['rawscss_desc'] = 'Use this field to provide SCSS or CSS code which will be injected at the end of the style sheet.';
+// Raw initial SCSS setting.                                                                                                        
+$string['rawscsspre'] = 'Raw initial SCSS';
+// Raw initial SCSS setting help text.                                                                                              
+$string['rawscsspre_desc'] = 'In this field you can provide initialising SCSS code, it will be injected before everything else. Most of the time you will use this setting to define variables.';
+// We need to include a lang string for each block region.                                                                          
+$string['region-side-pre'] = 'Right';
+
+// Background image for login page.                                                                                                 
+$string['loginbackgroundimage'] = 'Login page background image';
+// Background image for login page.                                                                                                 
+$string['loginbackgroundimage_desc'] = 'An image that will be stretched to fill the background of the login page.';
